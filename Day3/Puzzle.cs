8b5490bbe690514eeabc2d3385f28666965094f3
Dominik Marcinowski <dmarcinowski@zoho.com>
2022-12-03 15:17:06 +0100

    Add day 3

---
+++ Day3/Puzzle.cs
@@ -0,0 +1,63 @@
+﻿using static System.IO.File;
+
+namespace Day3;
+
+public class Puzzle
+{
+    private readonly List<string> _rucksacks = new();
+
+    public Puzzle(string file)
+    {
+        foreach (var rucksack in ReadLines(file))
+            _rucksacks.Add(rucksack);
+    }
+
+    public void Part1()
+    {
+        var sumOfPriorities = _rucksacks
+            .Select(FindSameItemInCompartments)
+            .Select(GetItemPriority)
+            .Sum();
+
+        Console.WriteLine(sumOfPriorities);
+    }
+
+    public void Part2()
+    {
+        const int elfGroupSize = 3;
+        var sumOfPriorities = 0;
+
+        for (var i = 0; i < _rucksacks.Count; i += elfGroupSize)
+        {
+            var rucksacks = _rucksacks.GetRange(i, elfGroupSize);
+            var sameItem = FindSameItemInThreeRucksacks(rucksacks);
+
+            sumOfPriorities += GetItemPriority(sameItem);
+        }
+
+        Console.WriteLine(sumOfPriorities);
+    }
+
+    private static char FindSameItemInCompartments(string items)
+    {
+        var firstCompartment = items[..(items.Length / 2)];
+        var secondCompartment = items[(items.Length / 2)..];
+
+        return FindSameItem(firstCompartment, secondCompartment)[0];
+    }
+
+    private static char FindSameItemInThreeRucksacks(IReadOnlyList<string> rucksacks)
+    {
+        var sameItemsInFirstTwo = FindSameItem(rucksacks[0], rucksacks[1]);
+        return sameItemsInFirstTwo.First(item => rucksacks[2].Contains(item));
+    }
+
+    private static string FindSameItem(string first, string second) =>
+        string.Concat(second.Where(first.Contains).Distinct());
+
+    private static int GetItemPriority(char item)
+    {
+        const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return letters.IndexOf(item) + 1;
+    }
+}
--- README.md
+++ README.md
@@ -5,7 +5,8 @@ I chose this language just because I wanted to learn more C# outside of Unity/Go
 
 ## Puzzles
 
-| Day   | Puzzle Name                                                |
-|-------|------------------------------------------------------------|
-| Day 1 | [Calorie Counting](https://adventofcode.com/2022/day/1)    |
-| Day 2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) |
+| Day   | Puzzle Name                                                    |
+|-------|----------------------------------------------------------------|
+| Day 1 | [Calorie Counting](https://adventofcode.com/2022/day/1)        |
+| Day 2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2)     |
+| Day 3 | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) |
