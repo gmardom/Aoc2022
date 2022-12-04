c6d02cf690c91710eeddafef2faf46f002c35fae
Dominik Marcinowski <dmarcinowski@zoho.com>
2022-12-04 12:08:35 +0100

    Add day 4

---
+++ Day4/Elf.cs
@@ -0,0 +1,26 @@
+﻿namespace Day4;
+
+public class Elf
+{
+    private readonly int _start;
+    private readonly int _end;
+
+    public Elf(string section)
+    {
+        var numbers = section.Split('-');
+        _start = int.Parse(numbers[0]);
+        _end = int.Parse(numbers[1]);
+    }
+
+    public bool FullyOverlaps(Elf other) =>
+        other._start <= _start && other._end >= _end;
+
+    public bool OverlapsOnEnd(Elf other) =>
+        other._start <= _start && (other._end >= _start && other._end <= _end);
+
+    public bool OverlapsOnStart(Elf other) =>
+        other._end >= _end && (other._start >= _start && other._end <= _end);
+
+    public bool OverlapsAny(Elf other) =>
+        FullyOverlaps(other) || OverlapsOnEnd(other) || OverlapsOnStart(other);
+}
--- README.md
+++ README.md
@@ -10,3 +10,4 @@ I chose this language just because I wanted to learn more C# outside of Unity/Go
 | Day 1 | [Calorie Counting](https://adventofcode.com/2022/day/1)        |
 | Day 2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2)     |
 | Day 3 | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) |
+| Day 4 | [Camp Cleanup](https://adventofcode.com/2022/day/4)            |
