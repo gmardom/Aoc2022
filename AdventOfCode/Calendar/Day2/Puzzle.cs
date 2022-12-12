5c6e97fe4f2767ffea3cfe58bde2055c60aec99e
Dominik Marcinowski <dmarcinowski@zoho.com>
2022-12-12 13:03:42 +0100

    Port Day 2

---
+++ AdventOfCode/Calendar/Day2/Puzzle.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Utils;
+using static AdventOfCode.Calendar.Day2.RockPaperScissors;
+
+namespace AdventOfCode.Calendar.Day2;
+
+[PuzzleSolution] public class Puzzle : Solution
+{
+    public Puzzle() : base(2)
+    { }
+
+    protected override void Part1()
+    {
+        var points = 0;
+
+        foreach (var line in Input.Lines())
+        {
+            var foes = ReadMove(line[0]);
+            var mine = ReadMove(line[2]);
+
+            points += CalculatePoints(foes, mine);
+        }
+
+        Print(1, points);
+    }
+
+    protected override void Part2()
+    {
+        var points = 0;
+
+        foreach (var line in Input.Lines())
+        {
+            var foes = ReadMove(line[0]);
+            var mine = MakeMove(foes, line[2]);
+
+            points += CalculatePoints(foes, mine);
+        }
+
+        Print(2, points);
+    }
+}
