666fe0ca0eea7138256cc08442e726ebc6b97331
Dominik Marcinowski <dmarcinowski@zoho.com>
2022-12-12 13:08:58 +0100

    Add Day 11

---
+++ AdventOfCode/Calendar/Day11/Monkey.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode.Calendar.Day11;
+
+public class Monkey
+{
+    private readonly Queue<long> _items;
+
+    private readonly string[] _operation;
+    private readonly bool _divideWorryLevel;
+
+    public readonly int Test;
+    private readonly int _testIfTrue;
+    private readonly int _testIfFalse;
+
+    public long Inspections { get; private set; }
+
+    public Monkey(IReadOnlyList<string> input, bool divideWorryLevel)
+    {
+        _items = new Queue<long>(input[1][17..].Split(", ").Select(long.Parse));
+
+        _operation = input[2][19..].Split(" ");
+        _divideWorryLevel = divideWorryLevel;
+
+        Test = int.Parse(input[3].Split(" ")[^1]);
+        _testIfTrue = int.Parse(input[4].Split(" ")[^1]);
+        _testIfFalse = int.Parse(input[5].Split(" ")[^1]);
+    }
+
+    public List<ValueTuple<int, long>> Throw(int divisorLimit)
+    {
+        var items = new List<(int, long)>();
+
+        while (_items.Any())
+        {
+            Inspections += 1;
+
+            var worryLevel = _items.Dequeue();
+            worryLevel = Operation(worryLevel);
+            worryLevel %= divisorLimit;
+
+            var targetMonkey = worryLevel % Test == 0 ? _testIfTrue : _testIfFalse;
+
+            items.Add((targetMonkey, worryLevel));
+        }
+
+        return items;
+    }
+
+    public void CatchItem(long item)
+    {
+        _items.Enqueue(item);
+    }
+
+    private long Operation(long item)
+    {
+        var op = _operation[1];
+        var lhs = _operation[0] == "old" ? item : int.Parse(_operation[0]);
+        var rhs = _operation[2] == "old" ? item : int.Parse(_operation[2]);
+
+        var value = op switch
+        {
+            "+" => lhs + rhs,
+            "*" => lhs * rhs,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        return _divideWorryLevel ? value / 3 : value;
+    }
+}
---
+++ AdventOfCode/Calendar/Day11/Puzzle.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode.Utils;
+
+namespace AdventOfCode.Calendar.Day11;
+
+[PuzzleSolution] public class Puzzle : Solution
+{
+    public Puzzle() : base(11)
+    { }
+
+    protected override void Part1()
+    {
+        var monkeys = Input.Lines()
+            .Chunk(6).Select(m => new Monkey(m, true)).ToList();
+
+        SimulateMonkeys(ref monkeys, 20);
+
+        Print(2, $"{monkeys[0].Inspections * monkeys[1].Inspections}");
+    }
+
+    protected override void Part2()
+    {
+        var monkeys = Input.Lines()
+            .Chunk(6).Select(m => new Monkey(m, false)).ToList();
+
+        SimulateMonkeys(ref monkeys, 10_000);
+
+        Print(2, $"{monkeys[0].Inspections * monkeys[1].Inspections}");
+    }
+
+    private static void SimulateMonkeys(ref List<Monkey> monkeys, int rounds)
+    {
+        var divisorLimit = monkeys.Aggregate(1, (c, m) => c * m.Test);
+
+        for (var i = 0; i < rounds; i++)
+            foreach (var monkey in monkeys)
+            foreach (var (to, item) in monkey.Throw(divisorLimit))
+                monkeys[to].CatchItem(item);
+
+        monkeys.Sort((a, b) => b.Inspections.CompareTo(a.Inspections));
+    }
+}
